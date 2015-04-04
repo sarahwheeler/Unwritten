fd7a10e665da15b480f0555c0a36f62faac31fa3
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-04-03 22:44:02 -0500

    Creates Character model in Rails, sets up for Ember app.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331033047) do
+ActiveRecord::Schema.define(version: 20150404034002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(version: 20150331033047) do
     t.string   "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "characters", force: true do |t|
+    t.string   "name"
+    t.string   "default_gender"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "book_id"
   end
 
 end
---
+++ spec/factories/characters.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :character do
+    name "MyString"
+default_gender "MyString"
+  end
+
+end
