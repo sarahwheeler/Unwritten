8e430b9c20b8cedfa7b8cdb43df7c7eff43d3426
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-04-13 23:54:23 -0500

    Adds db seeds and Moby Dick HTML.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+pride = Book.create(title: "Pride and Prejudice",
+										author: "Jane Austen",
+										text: "pride_and_prejudice.html")
+
+dorian = Book.create(title: "The Picture of Dorian Gray",
+											author: "Oscar Wilde",
+											text: "picture_of_dorian_gray.html")
+
+huckleberry = Book.create(title: "Adventures of Huckleberry Finn",
+										author: "Mark Twain",
+										text: "huckleberry_finn.html")
+
+moby = Book.create(title: "Moby Dick; Or, The Whale",
+										author: "Herman Melville",
+										text: "moby_dick.html")
