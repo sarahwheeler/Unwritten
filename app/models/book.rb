955c4e2d30cdd06640c64036a861630e35b5ffbe
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-04-13 23:45:43 -0500

    Adds associations for book and character.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
 	validates :title, uniqueness: true
 	validates :title, :author, :text, presence: true
+
+	has_many :characters
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,4 @@
 class Character < ActiveRecord::Base
+	belongs_to :book
+	has_one :pronoun
 end
