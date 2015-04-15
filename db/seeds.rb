1813a4fb10a0238f831da58fb6adedaa74f1eb08
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-04-14 19:56:33 -0500

    Creates seeds for db; general (but invisible) envisioning.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,61 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Books
+	pride = Book.create(title: "Pride and Prejudice",
+											author: "Jane Austen",
+											text: "pride_and_prejudice.html",
+											pub_year: "1813")
+	dorian = Book.create(title: "The Picture of Dorian Gray",
+												author: "Oscar Wilde",
+												text: "picture_of_dorian_gray.html",
+												pub_year: "1890")
+	huckleberry = Book.create(title: "Adventures of Huckleberry Finn",
+											author: "Mark Twain",
+											text: "huckleberry_finn.html",
+											pub_year: "1885")
+	moby = Book.create(title: "Moby Dick; Or, The Whale",
+											author: "Herman Melville",
+											text: "moby_dick.html",
+											pub_year: "1851")
 
-pride = Book.create(title: "Pride and Prejudice",
-										author: "Jane Austen",
-										text: "pride_and_prejudice.html")
 
-dorian = Book.create(title: "The Picture of Dorian Gray",
-											author: "Oscar Wilde",
-											text: "picture_of_dorian_gray.html")
+# Pronouns
+	feminine_trad = Pronoun.create(nominative: "she", oblique: "her", 
+													possessive: "hers",	possessive_det: "her", 
+													reflexive: "herself")
+	masculine_trad = Pronoun.create(nominative: "he", oblique: "him", 
+													possessive: "his",	possessive_det: "his", 
+													reflexive: "himself")
+	spivak = Pronoun.create(nominative: "e", oblique: "em", 
+													possessive: "eirs",	possessive_det: "eir", 
+													reflexive: "emself")
+	they = Pronoun.create(nominative: "they", oblique: "them", 
+													possessive: "theirs",	possessive_det: "their", 
+													reflexive: "themself")
+	ze1 = Pronoun.create(nominative: "ze", oblique: "zir", 
+													possessive: "zirs",	possessive_det: "zir", 
+													reflexive: "zirself")
+	ze2 = Pronoun.create(nominative: "ze", oblique: "zem", 
+												possessive: "zes",	possessive_det: "zes", 
+												reflexive: "zirself")
+	elverson = Pronoun.create(nominative: "ey", oblique: "em", 
+													possessive: "eirs",	possessive_det: "eir", 
+													reflexive: "eirself")
+	humanist = Pronoun.create(nominative: "hu", oblique: "hum", 
+													possessive: "hus",	possessive_det: "hus", 
+													reflexive: "humself")
 
-huckleberry = Book.create(title: "Adventures of Huckleberry Finn",
-										author: "Mark Twain",
-										text: "huckleberry_finn.html")
-
-moby = Book.create(title: "Moby Dick; Or, The Whale",
-										author: "Herman Melville",
-										text: "moby_dick.html")
+# Characters
+	# Pride and Prejudice
+		elizabeth = pride.characters.create(name: "Elizabeth Bennet", default_gender: :female)
+		darcy = pride.characters.create(name: "Fitzwilliam Darcy", default_gender: :male)
+		jane = pride.characters.create(name: "Jane Bennet", default_gender: :female)
+		bingley = pride.characters.create(name: "Charles Bingley", default_gender: :male)
+		mr_bennet = pride.characters.create(name: "Mr. Bennet", default_gender: :male)
+		mrs_bennet = pride.characters.create(name: "Mrs. Bennet", default_gender: :female)
+		wickham = pride.characters.create(name: "George Wickham", default_gender: :male)
+		lydia = pride.characters.create(name: "Lydia Bennet", default_gender: :female)
+		mr_collins = pride.characters.create(name: "Mr. Collins", default_gender: :male)
+		miss_bingley = pride.characters.create(name: "Miss Bingley", default_gender: :female)
+		catherine = pride.characters.create(name: "Lady Catherine de Bourgh", default_gender: :female)
